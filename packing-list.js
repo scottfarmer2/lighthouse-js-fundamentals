802aa720ce7e325764f0d0240aa4771bb553c8db
Scott Farmer <sfarmer664@gmail.com>
2016-12-09 19:12:19 +0000

    Adds looping exercises

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "cup", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
