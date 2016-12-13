8028e118a3bccb765dbdedeb127fa8a067168c0a
Scott Farmer <sfarmer664@gmail.com>
2016-12-13 15:45:55 +0000

    Adds loopylighthouse

---
+++ loopylighthouse.js
@@ -0,0 +1,13 @@
+for (var number = 100; number <= 200; number++) {
+  if (number % 3 === 0 && number % 4 === 0) {
+    console.log("LoopyLighthouse");
+  }
+  else if (number % 4 === 0) {
+    console.log("Lighthouse");
+  }
+  else if (number % 3 === 0) {
+    console.log("Loopy");
+  } else {
+    console.log(number);
+  }
+}
